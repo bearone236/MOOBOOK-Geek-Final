dec3153e3d2d2c009f6546d4ddbb9eb274eaafc1
bearone236 <kuma_2306@icloud.com>
2023-09-20 23:23:31 +0900

    API URLの変更

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { ImageInfo } from '../pages/Slider';
 
-const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
-
 // サーバーから画像データを取得する関数api
 export const fetchImages = (formData: FormData) => {
-  return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
+  return axios.post<ImageInfo[]>(`${process.env.REACT_APP_API_ENDPOINT}/upload`, formData);
 };
