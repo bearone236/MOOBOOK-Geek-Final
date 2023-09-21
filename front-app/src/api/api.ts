b26a6db0b6498d6464b85d569e67895097af7caf
bearone236 <kuma_2306@icloud.com>
2023-09-21 09:01:38 +0900

    .envファイルの移動

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 import { ImageInfo } from '../pages/Slider';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+console.log(apiEndpoint);
 
 // サーバーから画像データを取得する関数api
 export const fetchImages = (formData: FormData) => {
   return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
 };
-
-console.log(apiEndpoint);
