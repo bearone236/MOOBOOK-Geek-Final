9b77e617ddb376f1f0b4d991e60f3faaedd80e56
bearone236 <kuma_2306@icloud.com>
2023-09-21 09:35:01 +0900

    vite環境での.envの変数定義変更

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { ImageInfo } from '../pages/Slider';
 
-const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+const apiEndpoint = process.env.VITE_API_URL;
 console.log(apiEndpoint);
-console.log(process.env.REACT_APP_API_ENDPOINT);
+console.log(process.env.VITE_API_URL);
 // サーバーから画像データを取得する関数api
 export const fetchImages = (formData: FormData) => {
   return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
