45f6d30c5d877d90fa3dd0c57bfb23a6595107cb
bearone236 <kuma_2306@icloud.com>
2023-09-21 09:28:37 +0900

    viteファイルの変更

--- front-app/vite.config.ts
+++ front-app/vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      external: ['prop-types'],
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 3000,
     },
-  },
-});
+    build: {
+      rollupOptions: {
+        external: ['prop-types'],
+      },
+    },
+  });
+};
