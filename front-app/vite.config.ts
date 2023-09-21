dee68c150132fe413ee803f8ebd3eeb59b7b80e7
bearone236 <kuma_2306@icloud.com>
2023-09-21 23:38:27 +0900

    コメントアウト

--- front-app/vite.config.ts
+++ front-app/vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
     plugins: [react()],
-    // envDir: '../',
+    envDir: '../',
     server: {
       host: true,
       port: 3000,
