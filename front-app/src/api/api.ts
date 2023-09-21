771a658a9d67eb18fe5dd3ba8625a6a0aac6104e
bearone236 <kuma_2306@icloud.com>
2023-09-21 09:18:04 +0900

    Logの追加 / モニタリング

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -3,7 +3,7 @@ import { ImageInfo } from '../pages/Slider';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
 console.log(apiEndpoint);
-
+console.log(process.env.REACT_APP_API_ENDPOINT);
 // サーバーから画像データを取得する関数api
 export const fetchImages = (formData: FormData) => {
   return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
