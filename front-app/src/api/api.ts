50593fbeb193c39c045749943a1987fa17e5e261
bearone236 <kuma_2306@icloud.com>
2023-09-22 11:16:37 +0900

    ローカルの修正

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ImageInfo } from '../pages/Slider';
 
 const apiEndpoint = import.meta.env.VITE_API_URL;
-// サーバーから画像データを取得する関数api
+
 export const fetchImages = (formData: FormData) => {
   return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
 };
