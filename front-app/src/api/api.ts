9f30538d178a1df117fd521fffa26b8e8f8d4ad9
bearone236 <kuma_2306@icloud.com>
2023-09-22 13:05:12 +0900

    api.tsの変更

--- front-app/src/api/api.ts
+++ front-app/src/api/api.ts
@@ -2,7 +2,13 @@ import axios from 'axios';
 import { ImageInfo } from '../pages/Slider';
 
 const apiEndpoint = import.meta.env.VITE_API_URL;
+const axiosInstance = axios.create({
+  baseURL: apiEndpoint,
+});
 
+axiosInstance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+// 画像データの取得リクエストを行う関数
 export const fetchImages = (formData: FormData) => {
-  return axios.post<ImageInfo[]>(`${apiEndpoint}/upload`, formData);
+  return axiosInstance.post<ImageInfo[]>('/upload', formData);
 };
